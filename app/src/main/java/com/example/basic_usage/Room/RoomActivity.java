42eb699ef962062eb26bc0fcee49fe9b62c99313
ChaMinzi <ckalswl331@naver.com>
2020-08-16 19:49:35 +0900

    Using Livedata

--- app/src/main/java/com/example/basic_usage/Room/RoomActivity.java
+++ app/src/main/java/com/example/basic_usage/Room/RoomActivity.java
@@ -1,6 +1,7 @@
 package com.example.basic_usage.Room;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.room.Room;
 
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.basic_usage.R;
+
+import java.util.List;
 
 public class RoomActivity extends AppCompatActivity {
     private EditText mTodoEditText;
@@ -22,19 +25,31 @@ public class RoomActivity extends AppCompatActivity {
         mTodoEditText = findViewById(R.id.todo_edit);
         mResultTextView = findViewById(R.id.result_text);
 
+        /**
+         * main Thread에서 db 작업은 할 수 없다.
+         * allowMainThreadQueries 옵션은 임시방편이다.
+         * 만약 db 작업량이 많은 경우 main Thread를 사용하면 화면 draw 과정이 느려질 수 있다.
+         **/
         final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class,
                 "todo-db")
                 .allowMainThreadQueries()
                 .build();
 
-        mResultTextView.setText(db.todoDao().getAll().toString());
+        /**
+         * getAll()을 했을 때 변경된 인자가 todos로 들어온다.
+         * Lambda 사용
+         * project struct -> modules에서 target version을 1.8로 하면 람다식 사용 가능
+         *
+         * Live data 사용시
+         * 변경 사항이 있는지 관찰하다가 변경되면 알아서 UI 갱신
+         **/
+        db.todoDao().getAll().observe(this, todos -> {
+            mResultTextView.setText(todos.toString());
+        });
 
-        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
-                mResultTextView.setText(db.todoDao().getAll().toString());
-            }
+        // button 클릭 시 DB에 insert
+        findViewById(R.id.add_button).setOnClickListener(view -> {
+            db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
         });
     }
 }

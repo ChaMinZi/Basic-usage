d87dee59769d0b0884aec1a75fa906ccc463e989
ChaMinzi <ckalswl331@naver.com>
2021-01-11 23:09:18 +0900

    위치 설정 및 위험권한 체크 추가

--- MaskInfo/app/src/main/java/com/example/maskinfo/MainActivity.kt
+++ MaskInfo/app/src/main/java/com/example/maskinfo/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.maskinfo
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,9 +20,37 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
 
+    private val requestPermission = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()){ map ->
+        
+        // null이 들어오지 않는다고 강제
+        if (map[Manifest.permission.ACCESS_FINE_LOCATION]!! ||
+                map[Manifest.permission.ACCESS_COARSE_LOCATION]!!) {
+            viewModel.fetchStoreInfo()
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // permission check
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // 권한 요청하는 부분
+            requestPermission.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ))
+            return
+        }
 
         val storeAdapter = StoreAdapter()
 
@@ -36,9 +70,9 @@ class MainActivity : AppCompatActivity() {
                 storeAdapter.updateItems(stores)
             })
 
-            loadingLiveData.observe(this@MainActivity, {
+            loadingLiveData.observe(this@MainActivity) {
                 progressBar.visibility = if (it) View.VISIBLE else View.GONE
-            })
+            }
         }
     }
 }
--- MaskInfo/app/src/main/java/com/example/maskinfo/MainViewModel.kt
+++ MaskInfo/app/src/main/java/com/example/maskinfo/MainViewModel.kt
@@ -1,17 +1,23 @@
 package com.example.maskinfo
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.maskinfo.model.Store
 import com.example.maskinfo.repository.MaskService
+import com.google.android.gms.location.FusedLocationProviderClient
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainViewModel @ViewModelInject constructor(
-    private val service: MaskService
+    private val service: MaskService,
+    private val fusedLocationProviderClient: FusedLocationProviderClient
 ) : ViewModel() {
     val itemLiveData = MutableLiveData<List<Store>>()
     val loadingLiveData = MutableLiveData<Boolean>()
@@ -20,17 +26,22 @@ class MainViewModel @ViewModelInject constructor(
         fetchStoreInfo()
     }
 
+    @SuppressLint("MissingPermission")
     fun fetchStoreInfo() {
         // 로딩 시작
         loadingLiveData.value = true
 
-        viewModelScope.launch {
-            val storeInfo = service.fetchStoreInfo(37.188078, 127.043002)
-            itemLiveData.value = storeInfo.stores.filter { store ->
-                store.remain_stat != null
-            }
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            viewModelScope.launch {
+                val storeInfo = service.fetchStoreInfo(location.latitude, location.longitude)
+                itemLiveData.value = storeInfo.stores.filter { store ->
+                    store.remain_stat != null
+                }
 
-            // 로딩 끝
+                // 로딩 끝
+                loadingLiveData.value = false
+            }
+        }.addOnFailureListener { exception ->
             loadingLiveData.value = false
         }
     }
